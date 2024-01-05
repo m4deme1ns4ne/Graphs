f70727a95aae502acf4d5bea68fd0987612418f9
m4deme1ns4ne <alexandervolzhanin2004@gmail.com>
2024-01-05 17:16:38 +0300

    first commit

---
+++ my_mistakes.py
@@ -0,0 +1,7 @@
+class Much_X(Exception):
+    def init(self, *args: object) -> None:
+        super().init(*args)
+
+class Much_Y(Exception):
+    def init(self, *args: object) -> None:
+        super().init(*args)
